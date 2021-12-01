4f0355320489918a282e231b2f179348386367b4
Samit Koyom <samitkoyom@gmail.com>
2021-12-01 16:40:43 +0700

    init commit

---
+++ MVCLayout/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MVCLayout
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include("~/Scripts/scripts.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/styles.css"));
+        }
+    }
+}
